56a4050474ec5118bff198bb930d94ad8a36e2f5
goremi <ffdan@naver.com>
2025-02-20 14:41:13 +0900

    second

--- main.py
+++ main.py
@@ -12,27 +12,33 @@ drone3.open('COM5')
 
 while True:
     if keyboard.is_pressed('enter'):
-        print('순차 이륙')
+        print('이륙')
         sleep(2)
         drone1.sendTakeOff()
-        sleep(1)
         drone2.sendTakeOff()
-        sleep(1)
         drone3.sendTakeOff()
         sleep(5)
         print('이륙 완료')
     if keyboard.is_pressed('space'):
-        print('순차 착륙')
+        print('착륙')
         drone1.sendControlWhile(0, 0, 0, 0, 500)
         drone2.sendControlWhile(0, 0, 0, 0, 500)
         drone3.sendControlWhile(0, 0, 0, 0, 500)
         drone1.sendLanding()
-        sleep(1)
         drone2.sendLanding()
-        sleep(1)
         drone3.sendLanding()
         sleep(3)
         print('착륙 완료')
     if keyboard.is_pressed('esc'):
         print('프로그램 종료')
         break
+    if keyboard.is_pressed('1'):
+        for i in range(3):
+            drone1.sendControlPosition(0, 1, 0, 1, 0, 0)
+            drone2.sendControlPosition(0, 0, 1, 1, 0, 0)
+            drone3.sendControlPosition(0, -1, 0, 1, 0, 0)
+            sleep(2)
+            drone1.sendControlPosition(0, -1, 0, 1, 0, 0)
+            drone2.sendControlPosition(0, 0, -1, 1, 0, 0)
+            drone3.sendControlPosition(0, 1, 0, 1, 0, 0)
+            sleep(2)
